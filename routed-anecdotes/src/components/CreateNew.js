a1554994559ec05c3f4599b8523409e4368fe350
jcLucio <c.lucio3@gmail.com>
2021-06-30 21:41:23 -0500

    Add anecdotes and hooks exercise (7.5) - Add reset function to useField custom hook - Add handleReset and reset button to CreateNew component

--- routed-anecdotes/src/components/CreateNew.js
+++ routed-anecdotes/src/components/CreateNew.js
@@ -15,6 +15,12 @@ const CreateNew = (props) => {
     });
   };
 
+  const handleReset = () => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -31,7 +37,10 @@ const CreateNew = (props) => {
           url for more info
           <input name="info" {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
