0a2e04ed76e69224590b9b75286bddb215cc085b
jcLucio <c.lucio3@gmail.com>
2021-07-09 22:52:02 -0500

    Add styles exercise (7.19) - Install @material-ui/core - Add roboto font cdn as link tag - Add body to index.css for font - Style App component - Style Menu component

--- bloglist/bloglist-frontend/src/App.js
+++ bloglist/bloglist-frontend/src/App.js
@@ -12,8 +12,24 @@ import UserList from './components/UserList';
 import Togglable from './components/Togglable';
 import { initializeBlogs } from './reducers/blogReducer';
 import { initializeUser } from './reducers/loginReducer';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: '#100723',
+    height: '100vh',
+    color: '#c7c3d2',
+    '& h2': {
+      margin: '0',
+      padding: '1rem 0',
+    },
+  },
+});
 
 const App = () => {
+  const classes = useStyles();
+
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const blogFormRef = useRef();
@@ -25,42 +41,44 @@ const App = () => {
 
   if (!user) {
     return (
-      <div>
+      <Container className={classes.root}>
         <h2>log in to application</h2>
         <Notification />
         <LoginForm />
-      </div>
+      </Container>
     );
   }
 
   return (
-    <div>
+    <div className={classes.root}>
       <Menu />
-      <h2>blog app</h2>
-      <Notification />
+      <Container>
+        <h2>blog app</h2>
+        <Notification />
 
-      <Switch>
-        <Route path="/users/:id">
-          <User />
-        </Route>
+        <Switch>
+          <Route path="/users/:id">
+            <User />
+          </Route>
 
-        <Route path="/blogs/:id">
-          <Blog />
-        </Route>
+          <Route path="/blogs/:id">
+            <Blog />
+          </Route>
 
-        <Route path="/users">
-          <UserList />
-        </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
 
-        <Route path="/">
-          <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-            <BlogForm
-              toggleForm={() => blogFormRef.current.toggleVisibility()}
-            />
-          </Togglable>
-          <BlogList />
-        </Route>
-      </Switch>
+          <Route path="/">
+            <Togglable buttonLabel="create new blog" ref={blogFormRef}>
+              <BlogForm
+                toggleForm={() => blogFormRef.current.toggleVisibility()}
+              />
+            </Togglable>
+            <BlogList />
+          </Route>
+        </Switch>
+      </Container>
     </div>
   );
 };
--- bloglist/bloglist-frontend/src/components/Menu.js
+++ bloglist/bloglist-frontend/src/components/Menu.js
@@ -2,26 +2,54 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../reducers/loginReducer';
+import { AppBar, Button, Container, Toolbar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: '#00000b',
+  },
+  user: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    margin: '0',
+    padding: '0.6rem 0',
+  },
+  logoutBtn: {
+    margin: '0 0.6rem',
+  },
+});
 
 const Menu = () => {
+  const classes = useStyles();
+
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
 
-  const style = {
-    backgroundColor: 'lightgrey',
-    padding: 5,
-    margin: 5,
-  };
-
   return (
-    <nav style={style}>
-      <Link to="/">blogs</Link>
-      <Link to="/users">users</Link>
-      <span>
-        {user.name} logged in
-        <button onClick={() => dispatch(logout())}>logout</button>
-      </span>
-    </nav>
+    <AppBar className={classes.root} position="relative">
+      <Toolbar>
+        <Button color="inherit" component={Link} to="/">
+          blogs
+        </Button>
+        <Button color="inherit" component={Link} to="/users">
+          users
+        </Button>
+        <Container className={classes.user} maxWidth="xl">
+          {user.name} logged in
+          <Button
+            className={classes.logoutBtn}
+            color="secondary"
+            variant="outlined"
+            size="small"
+            onClick={() => dispatch(logout())}
+          >
+            logout
+          </Button>
+        </Container>
+      </Toolbar>
+    </AppBar>
   );
 };
 
